b014108cda10c9c33ea2bd7d6fb6af2376ae8f52
rahulrt2005 <rahul_rt2005@yahoo.com>
2021-09-08 11:49:51 +0530

    Add files via upload

---
+++ bs/steps/steps_bs5.py
@@ -0,0 +1,48 @@
+import requests
+import subprocess
+import time
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        print(f"Time Taken to execute {method.__name__} method ::: {(te - ts)} seconds")
+        return result
+    return timed
+
+
+def download_bs5():
+    """
+    This method will download bs5 installer and return the directory path and downloaded file name
+    :return: dictionary with file name and path of downloaded file
+    """
+    bs5_download_url = "https://cdn3.bluestacks.com/downloads/windows/nxt/5.2.110.1003" \
+                       "/4b37f8a27d79ab796aef0455dcca9eb4/x64/BlueStacksFullInstaller_5.2.110.1003_amd64_native.exe"
+
+    file_path = "D:\\HackerRank\\bs5_installer.exe"
+    file_name = file_path.split("\\")[-1]
+    r = requests.get(bs5_download_url, stream=True)
+    print("Downloading Bluestacks 5 offline installer !!!!! ")
+    with open(file_path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+    print("Downloading Bluestacks 5 offline installer finished !!!!! ")
+    return {"file_name": file_name, 'path': file_path}
+
+
+@timeit
+def install_bs5(file_path):
+    """
+
+    :param file_path: Path of file to be installed
+    :return: Install Directory
+    """
+    cmd = f"{file_path} -s"
+    try:
+        subprocess.check_output(cmd)
+        print(f"BlueStacks 5 installation complete")
+    except:
+        print(f"Error occurred while installing")
